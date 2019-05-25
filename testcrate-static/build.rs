b116785cd046d2583c369f4ffa8e3921185c8ee6
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-05-25 08:27:37 -0400

    Test debug and release on CI

--- testcrate-static/build.rs
+++ testcrate-static/build.rs
@@ -1,6 +1,10 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
-    let artifacts = zeromq_src::Build::new().link_static(true).build();
+    let wants_debug = cfg!(debug_assertions);
+    let artifacts = zeromq_src::Build::new()
+        .build_debug(wants_debug)
+        .link_static(true)
+        .build();
     artifacts.print_cargo_metadata();
 }
