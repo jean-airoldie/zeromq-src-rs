9e848abb54fd6571c688695f6a7867379558a56f
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-06-12 11:39:22 -0400

    Add support for libsodium
    Updated travis script to test libsodium compilation
    Updated appveyor to test libsodium compilation

---
+++ testcrate/build.rs
@@ -0,0 +1,27 @@
+use std::env;
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed=PROFILE");
+
+    let wants_debug = env::var("PROFILE").unwrap() == "debug";
+    let wants_static = cfg!(feature = "static");
+
+    let maybe_libsodium = if cfg!(feature = "libsodium") {
+        let lib_dir = env::var("DEP_SODIUM_LIB")
+            .expect("build metadata `DEP_SODIUM_LIB` required");
+        let include_dir = env::var("DEP_SODIUM_INCLUDE")
+            .expect("build metadata `DEP_SODIUM_INCLUDE` required");
+
+        Some(zeromq_src::LibLocation::new(lib_dir, include_dir))
+    } else {
+        None
+    };
+
+    let artifacts = zeromq_src::Build::new()
+        .build_debug(wants_debug)
+        .link_static(wants_static)
+        .with_libsodium(maybe_libsodium)
+        .build();
+    artifacts.print_cargo_metadata();
+}
