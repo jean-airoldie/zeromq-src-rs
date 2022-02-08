88aabf2bcd0d967a077ea9f52ecc6d49bb4a4166
Jasper Bekkers <bekkers@gmail.com>
2022-02-08 22:46:51 +0100

    Switch to libsodium-sys-stable for tests

--- testcrate/src/lib.rs
+++ testcrate/src/lib.rs
@@ -1,8 +1,13 @@
 use std::os::raw::{c_int, c_void};
+#[cfg(feature = "libsodium")]
+use std::{ffi::CStr, os::raw::c_char};
 
 extern "C" {
     fn zmq_version(major: *mut i32, minor: *mut i32, patch: *mut i32)
         -> c_void;
+
+    #[cfg(feature = "libsodium")]
+    fn sodium_version_string() -> *const c_char;
 }
 
 pub fn version() -> (i32, i32, i32) {
@@ -20,14 +25,27 @@ pub fn version() -> (i32, i32, i32) {
     (major, minor, patch)
 }
 
+#[cfg(feature = "libsodium")]
+pub fn sodium_version() -> &'static CStr {
+    unsafe { CStr::from_ptr(sodium_version_string()) }
+}
+
 #[cfg(test)]
 mod test {
-    use super::version;
+    use super::*;
 
     #[test]
     fn version_works() {
         let version = version();
         println!("{:?}", version);
         assert_eq!(version, (4, 3, 4));
+    }
+
+    #[test]
+    #[cfg(feature = "libsodium")]
+    fn sodium_version_works() {
+        let version = sodium_version();
+        println!("{:?}", version.to_str().unwrap());
+        assert_eq!(version.to_str().unwrap(), "1.0.18");
     }
 }
