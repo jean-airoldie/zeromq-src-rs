649b49ef54f1f2f7feb311876d62401f2632a1c8
Jasper Bekkers <Jasper-Bekkers@users.noreply.github.com>
2022-02-14 02:09:29 +0100

    Remove `cmake` (#17)
    * Strip cmake
    * Update zmq to 4.3.4 to solve zeromq/libzmq/issues/3586
    * Switch to github actions for all CI
    * Add libsodium test
    * Remove perftools
    * Remove static feature
    
    Co-authored-by: Marijn Suijten <marijn@traverseresearch.nl>

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,42 @@
-use cmake::Config;
-
 use std::{
     env, fmt, fs,
+    fs::File,
+    io::Write,
     path::{Path, PathBuf},
 };
 
 pub fn source_dir() -> PathBuf {
     Path::new(env!("CARGO_MANIFEST_DIR")).join("vendor")
+}
+
+fn add_cpp_sources(
+    build: &mut cc::Build,
+    root: impl AsRef<Path>,
+    files: &[&str],
+) {
+    let root = root.as_ref();
+    build.files(files.iter().map(|src| {
+        let mut p = root.join(src);
+        p.set_extension("cpp");
+        p
+    }));
+
+    build.include(root);
+}
+
+fn add_c_sources(
+    build: &mut cc::Build,
+    root: impl AsRef<Path>,
+    files: &[&str],
+) {
+    let root = root.as_ref();
+    build.files(files.iter().map(|src| {
+        let mut p = root.join(src);
+        p.set_extension("c");
+        p
+    }));
+
+    build.include(root);
 }
 
 // Returns Ok(()) is file was renamed,
@@ -31,42 +61,14 @@ where
 }
 
 #[derive(Debug, Clone)]
-pub struct Artifacts {
-    include_dir: PathBuf,
-    lib_dir: PathBuf,
-    out_dir: PathBuf,
-    pkg_config_dir: PathBuf,
-    libs: Vec<Lib>,
-}
+pub struct Artifacts {}
 
 impl Artifacts {
-    pub fn include_dir(&self) -> &Path {
-        &self.include_dir
-    }
-
-    pub fn lib_dir(&self) -> &Path {
-        &self.lib_dir
-    }
-    pub fn pkg_config_dir(&self) -> &Path {
-        &self.pkg_config_dir
-    }
-
-    pub fn out_dir(&self) -> &Path {
-        &self.out_dir
-    }
-
-    pub fn libs(&self) -> &[Lib] {
-        &self.libs
-    }
-
     pub fn print_cargo_metadata(&self) {
-        println!("cargo:rustc-link-search=native={}", self.lib_dir.display());
-        for lib in self.libs.iter() {
-            println!("cargo:rustc-link-lib={}{}", lib.link_type, lib.name);
+        let target = env::var("TARGET").unwrap();
+        if target.contains("windows") {
+            println!("cargo:rustc-link-lib=dylib=iphlpapi");
         }
-        println!("cargo:include={}", self.include_dir.display());
-        println!("cargo:lib={}", self.lib_dir.display());
-        println!("cargo:out={}", self.out_dir.display());
     }
 }
 
@@ -84,37 +86,6 @@ impl fmt::Display for LinkType {
             LinkType::Static => write!(f, "static="),
             LinkType::Unspecified => write!(f, ""),
         }
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum BuildType {
-    Release,
-    Debug,
-}
-
-#[derive(Debug, Clone)]
-pub struct Lib {
-    name: String,
-    link_type: LinkType,
-}
-
-impl Lib {
-    fn new<S>(name: S, link_type: LinkType) -> Self
-    where
-        S: Into<String>,
-    {
-        let name = name.into();
-
-        Self { name, link_type }
-    }
-
-    pub fn name(&self) -> &str {
-        &self.name
-    }
-
-    pub fn link_type(&self) -> LinkType {
-        self.link_type
     }
 }
 
@@ -150,8 +121,6 @@ pub struct Build {
     enable_draft: bool,
     enable_curve: bool,
     build_debug: bool,
-    link_static: bool,
-    perf_tool: bool,
     libsodium: Option<LibLocation>,
 }
 
@@ -161,16 +130,8 @@ impl Build {
             enable_draft: false,
             enable_curve: true,
             build_debug: false,
-            link_static: false,
-            perf_tool: false,
             libsodium: None,
         }
-    }
-
-    /// Build & link statically instead of dynamically.
-    pub fn link_static(&mut self, enabled: bool) -> &mut Self {
-        self.link_static = enabled;
-        self
     }
 
     /// Build the debug version of the lib.
@@ -191,12 +152,6 @@ impl Build {
         self
     }
 
-    /// Build with perf-tools.
-    pub fn perf_tool(&mut self, enabled: bool) -> &mut Self {
-        self.perf_tool = enabled;
-        self
-    }
-
     /// Use an external `libsodium` library instead of `tweenacl`.
     ///
     /// Users can link against an installed lib or another `sys` or `src` crate
@@ -213,108 +168,236 @@ impl Build {
     /// Returns an `Artifacts` which contains metadata for linking
     /// against the compiled lib from rust code.
     pub fn build(&mut self) -> Artifacts {
-        let mut config = Config::new(source_dir());
+        let path = source_dir();
 
-        config
-            // For the LIBDIR to always be `lib`.
-            .define("CMAKE_INSTALL_LIBDIR", "lib")
-            // `libzmq` uses C99 but doesn't specify it.
-            .define("CMAKE_C_STANDARD", "99")
-            .define("ZMQ_BUILD_TESTS", "OFF");
+        let mut build = cc::Build::new();
+        build
+            .cpp(true)
+            .define("ZMQ_BUILD_TESTS", "OFF")
+            .include(path.join("include"))
+            .include(path.join("src"));
+
+        add_cpp_sources(
+            &mut build,
+            path.join("src"),
+            &[
+                "address",
+                "channel",
+                "client",
+                "clock",
+                "ctx",
+                "curve_client",
+                "curve_mechanism_base",
+                "curve_server",
+                "dealer",
+                "decoder_allocators",
+                "devpoll",
+                "dgram",
+                "dish",
+                "dist",
+                "endpoint",
+                "epoll",
+                "err",
+                "fq",
+                "gather",
+                "gssapi_client",
+                "gssapi_mechanism_base",
+                "gssapi_server",
+                "io_object",
+                "io_thread",
+                "ip_resolver",
+                "ip",
+                "ipc_address",
+                "ipc_connecter",
+                "ipc_listener",
+                "kqueue",
+                "lb",
+                "mailbox_safe",
+                "mailbox",
+                "mechanism_base",
+                "mechanism",
+                "metadata",
+                "msg",
+                "mtrie",
+                "norm_engine",
+                "null_mechanism",
+                "object",
+                "options",
+                "own",
+                "pair",
+                "peer",
+                "pgm_receiver",
+                "pgm_sender",
+                "pgm_socket",
+                "pipe",
+                "plain_client",
+                "plain_server",
+                "poll",
+                "poller_base",
+                "polling_util",
+                "pollset",
+                "precompiled",
+                "proxy",
+                "pub",
+                "pull",
+                "push",
+                "radio",
+                "radix_tree",
+                "random",
+                "raw_decoder",
+                "raw_encoder",
+                "raw_engine",
+                "reaper",
+                "rep",
+                "req",
+                "router",
+                "scatter",
+                "select",
+                "server",
+                "session_base",
+                "signaler",
+                "socket_base",
+                "socket_poller",
+                "socks_connecter",
+                "socks",
+                "stream_connecter_base",
+                "stream_engine_base",
+                "stream_listener_base",
+                "stream",
+                "sub",
+                "tcp_address",
+                "tcp_connecter",
+                "tcp_listener",
+                "tcp",
+                "thread",
+                "timers",
+                "tipc_address",
+                "tipc_connecter",
+                "tipc_listener",
+                "trie",
+                "udp_address",
+                "udp_engine",
+                "v1_decoder",
+                "v1_encoder",
+                "v2_decoder",
+                "v2_encoder",
+                "v3_1_encoder",
+                "vmci_address",
+                "vmci_connecter",
+                "vmci_listener",
+                "vmci",
+                "ws_address",
+                "ws_connecter",
+                "ws_decoder",
+                "ws_encoder",
+                "ws_engine",
+                "ws_listener",
+                // "wss_address", // requires gnutls
+                // "wss_engine", // requires gnutls
+                "xpub",
+                "xsub",
+                "zap_client",
+                "zmq_utils",
+                "zmq",
+                "zmtp_engine",
+            ],
+        );
+
+        add_c_sources(&mut build, path.join("external/sha1"), &["sha1.c"]);
 
         if self.enable_draft {
-            config.define("ENABLE_DRAFTS", "ON");
-        } else {
-            config.define("ENABLE_DRAFTS", "OFF");
+            build.define("ZMQ_BUILD_DRAFT_API", "1");
         }
 
-        if self.enable_curve {
-            config.define("ENABLE_CURVE", "ON");
-        } else {
-            config.define("ENABLE_CURVE", "OFF");
-        }
+        build.define("ZMQ_USE_CV_IMPL_STL11", "1");
+        build.define("ZMQ_STATIC", "1");
+        build.define("ZMQ_USE_BUILTIN_SHA1", "1");
 
-        if self.build_debug {
-            config.define("CMAKE_BUILD_TYPE", "Debug");
-        } else {
-            config.define("CMAKE_BUILD_TYPE", "Release");
-        }
-
-        if self.perf_tool {
-            config.define("WITH_PERF_TOOL", "ON");
-        } else {
-            config.define("WITH_PERF_TOOL", "OFF");
-        }
+        build.define("ZMQ_HAVE_WS", "1");
 
         let target = env::var("TARGET").unwrap();
 
-        let link_type = {
-            if self.link_static {
-                config
-                    .define("BUILD_SHARED", "OFF")
-                    .define("BUILD_STATIC", "ON");
+        if let Some(libsodium) = &self.libsodium {
+            build.define("ZMQ_USE_LIBSODIUM", "1");
+            build.define("ZMQ_HAVE_CURVE", "1");
 
-                if target.contains("msvc") {
-                    config.cflag("-DZMQ_STATIC");
-                }
-
-                LinkType::Static
-            } else {
-                config
-                    .define("BUILD_SHARED", "ON")
-                    .define("BUILD_STATIC", "OFF");
-
-                LinkType::Dynamic
-            }
-        };
-
-        if target.contains("msvc") && link_type == LinkType::Dynamic {
-            panic!("dynamic compilation is currently not supported on windows");
-        }
-
-        let mut libs = vec![];
-
-        libs.push(Lib::new("zmq", link_type));
-
-        if let Some(ref location) = self.libsodium {
-            config.define("WITH_LIBSODIUM", "ON");
-
-            config.define("SODIUM_LIBRARIES", location.lib_dir());
-            config.define("SODIUM_INCLUDE_DIRS", location.include_dir());
+            build.include(libsodium.include_dir());
+            println!("cargo:rustc-link-search={:?}", libsodium.lib_dir());
 
             if target.contains("msvc") {
-                libs.push(Lib::new("libsodium", LinkType::Static));
-            } else {
-                libs.push(Lib::new("sodium", LinkType::Unspecified));
+                std::fs::copy(
+                    libsodium
+                        .include_dir()
+                        .join("../../../builds/msvc/version.h"),
+                    libsodium.include_dir().join("sodium/version.h"),
+                )
+                .unwrap();
             }
-        } else {
-            config.define("WITH_LIBSODIUM", "OFF");
+
+            if target.contains("msvc") {
+                println!("cargo:rustc-link-lib=static=libsodium");
+            } else {
+                println!("cargo:rustc-link-lib=sodium");
+            }
         }
 
-        if target.contains("apple")
-            || target.contains("freebsd")
-            || target.contains("openbsd")
-        {
-            libs.push(Lib::new("c++", LinkType::Dynamic));
+        let create_platform_hpp_shim = |build: &mut cc::Build| {
+            // https://cmake.org/cmake/help/latest/command/configure_file.html
+            // TODO: Replace `#cmakedefine` with the appropriate `#define`
+            // let _platform_file =
+            //     std::fs::read_to_string(path.join("builds/cmake/platform.hpp.in"))
+            //         .unwrap();
+
+            let out_includes = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+
+            // Write out an empty platform file: defines will be set through cc directly,
+            // sync to prevent potential IO troubles later on
+            let mut f =
+                File::create(out_includes.join("platform.hpp")).unwrap();
+            f.write_all(b"").unwrap();
+            f.sync_all().unwrap();
+
+            build.include(out_includes);
+        };
+
+        if target.contains("windows") {
+            build.define("ZMQ_IOTHREAD_POLLER_USE_SELECT", "1");
+            build.define("ZMQ_POLL_BASED_ON_SELECT", "1");
+            build.define("_WIN32_WINNT", "0x0600"); // vista
+            println!("cargo:rustc-link-lib=iphlpapi");
+
+            if target.contains("msvc") {
+                build.include(path.join("builds/deprecated-msvc"));
+                // We need to explicitly disable `/GL` flag, otherwise
+                // we get linkage error.
+                build.flag("/GL-");
+
+                // Fix warning C4530: "C++ exception handler used, but unwind
+                // semantics are not enabled. Specify /EHsc"
+                build.flag("/EHsc");
+            } else {
+                create_platform_hpp_shim(&mut build);
+                build.define("HAVE_STRNLEN", "1");
+            }
         } else if target.contains("linux") {
-            libs.push(Lib::new("stdc++", LinkType::Dynamic));
-        } else if target.contains("msvc") {
-            libs.push(Lib::new("iphlpapi", LinkType::Dynamic));
+            create_platform_hpp_shim(&mut build);
+            build.define("ZMQ_IOTHREAD_POLLER_USE_EPOLL", "1");
+            build.define("ZMQ_POLL_BASED_ON_POLL", "1");
+
+            build.define("HAVE_STRNLEN", "1");
+            build.define("ZMQ_HAVE_UIO", "1");
+        } else if target.contains("apple") {
+            create_platform_hpp_shim(&mut build);
+            build.define("ZMQ_IOTHREAD_POLLER_USE_KQUEUE", "1");
+            build.define("ZMQ_POLL_BASED_ON_POLL", "1");
+            build.define("HAVE_STRNLEN", "1");
+            build.define("ZMQ_HAVE_STRLCPY", "1");
+            build.define("ZMQ_HAVE_UIO", "1");
         }
 
-        if target.contains("msvc") {
-            // We need to explicitly disable `/GL` flag, otherwise
-            // we get linkage error.
-            config.cxxflag("/GL-");
-            // Fix warning C4530: "C++ exception handler used, but unwind
-            // semantics are not enabled. Specify /EHsc"
-            config.cxxflag("/EHsc");
-        }
-
-        let out_dir = config.build();
-        let lib_dir = out_dir.join("lib");
-        let include_dir = out_dir.join("include");
-        let pkg_config_dir = lib_dir.join("pkgconfig");
+        build.compile("zmq");
+        let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+        let lib_dir = out_dir.join("");
 
         // On windows we need to rename the static compiled lib
         // since its name is unpredictable.
@@ -323,14 +406,7 @@ impl Build {
         {
             panic!("unable to find compiled `libzmq` lib");
         }
-
-        Artifacts {
-            out_dir,
-            lib_dir,
-            include_dir,
-            pkg_config_dir,
-            libs,
-        }
+        Artifacts {}
     }
 }
 
--- testcrate/build.rs
+++ testcrate/build.rs
@@ -5,8 +5,6 @@ fn main() {
     println!("cargo:rerun-if-env-changed=PROFILE");
 
     let wants_debug = env::var("PROFILE").unwrap() == "debug";
-    let wants_static = cfg!(feature = "static");
-
     let maybe_libsodium = if cfg!(feature = "libsodium") {
         let lib_dir = env::var("DEP_SODIUM_LIB")
             .expect("build metadata `DEP_SODIUM_LIB` required");
@@ -18,10 +16,8 @@ fn main() {
         None
     };
 
-    let artifacts = zeromq_src::Build::new()
+    zeromq_src::Build::new()
         .build_debug(wants_debug)
-        .link_static(wants_static)
         .with_libsodium(maybe_libsodium)
         .build();
-    artifacts.print_cargo_metadata();
 }
