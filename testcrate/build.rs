4d41819f053863c852a2717af7b1f2e67237ad8c
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2025-03-05 12:09:44 -0500

    Fix typo

--- testcrate/build.rs
+++ testcrate/build.rs
@@ -10,7 +10,7 @@ fn main() {
         println!("cargo:warning=DEP_SODIUM_LIB={}", lib_dir);
         let include_dir = env::var("DEP_SODIUM_INCLUDE")
             .expect("build metadata `DEP_SODIUM_INCLUDE` required");
-        println!("cargo:warning=DEP_SODIUM_INCLUDE={}", lib_dir);
+        println!("cargo:warning=DEP_SODIUM_INCLUDE={}", include_dir);
 
         Some(zeromq_src::LibLocation::new(lib_dir, include_dir))
     } else {
